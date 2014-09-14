343639e2b0765026723d6d8a9555552ab5baa066
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2014-09-14 12:56:16 -0400

    start of app development

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 var express = require('express');
 var app = express();
+
+app.set("views", "./views");
+app.set("view engine", "jade");
+app.use(express.static("public"));
+
+app.get('/', function(req, res) {
+    res.render('index', { title: "Helo", haaay: "heyo" });
+});
