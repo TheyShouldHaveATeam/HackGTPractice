b3b2b692834f4d46db5e3cbcd5992fbd986bb357
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2014-09-14 13:18:04 -0400

    its on github, gawd

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ app.set("views", "./views");
 app.set("view engine", "jade");
 app.use(express.static("public"));
 
+app.listen(3000)
+
 app.get('/', function(req, res) {
-    res.render('index', { title: "Helo", haaay: "heyo" });
+    res.render('index', { title: "Helo", haaay: "<a href='yes.com'>heloa</a>" });
 });
