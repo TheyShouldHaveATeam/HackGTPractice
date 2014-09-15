c6a1bcf0c35c9a2967d4ec8c5f22a41006a11ecd
Raphael Gontijo Lopes <raphaelgontijolopes@gmail.com>
2014-09-15 19:54:41 -0400

    connects to database (proof of concept)

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+var util = require('util');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+
+// var mongoose = require("mongoose");
+// mongoose.connect("mongodb://localhost/TSHTTest");
+// var db = mongoose.connection;
 
 var UserStore = require('./models/User.js');
 var PostStore = require('./models/Post.js');
@@ -15,12 +20,29 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.listen(3000)
+app.listen(3000);
+
+var MongoClient = require("mongodb").MongoClient;
+
+function getFirstUserName() {
+    MongoClient.connect("mongodb://localhost:27017/TSHTTest", function(err, db) {
+        if(err) {
+            throw err;
+        }
+
+        db.collection("users").findOne({name: "hey"}, function (err, user) {
+            db.close();
+            return user.name;
+        });
+    });
+}
 
 app.get('/', function(req, res) {
-    res.render('index', { title: "Helo", haaay: "<a href='yes.com'>heloa</a>" });
+    var firstUserName = getFirstUserName();
+
+    res.render('index', { title: "Text Enterer", firstPost: firstUserName });
 });
 
 app.post('/', function(req, res) {
-    res.render('index', { title: "Helo", haaay: "<a href='yes.com'>heloa</a>" });
+    res.render('index', { title: "Text Enterer" });
 });
